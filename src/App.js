cb2e024e96cf9954de63b758effb460137f24c01
danieljporter21 <danieljporter21@googlemail.com>
2022-04-29 16:23:52 +0100

    refactored to add margin

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Jumbo from './Component/Jumbo'
+import Jumbo from './Components/Jumbo'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <div className="m-3">
       <Jumbo />
+    </div>
       <Footer />
     </div>
   );
