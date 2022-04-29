77ce11f38ed9f8241c982c82af11177566916a18
patrick-holden <holden.patrick@gmail.com>
2022-04-29 15:37:05 +0100

    remove anchor tag in footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -2,9 +2,7 @@ const Footer = () => {
   return (
     <div className="px-3">
       <footer className="d-flex flex-wrap px-3 py-2 my-2 border-top border-1">
-          <a href="" className="text-decoration-none lh-1">
-          </a>
-          <span className="text small">© Copyright iO Academy 2022</span>
+          <p className="text small">© Copyright iO Academy 2022</p>
       </footer>
     </div>
   );
