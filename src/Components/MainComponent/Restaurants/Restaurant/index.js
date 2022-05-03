1bad64b1970af010a25baa61a218b9b02ae87f52
alech <hamiltonalec@gmail.com>
2022-05-03 09:55:05 +0100

    pr changes

--- src/Components/MainComponent/Restaurants/Restaurant/index.js
+++ src/Components/MainComponent/Restaurants/Restaurant/index.js
@@ -5,7 +5,7 @@ const Restaurant = (props) => {
     return (
         <div className="text-center card border-1 border-info rounded-1 my-2 mx-3 mx-lg-1">
             <div>
-                <h2 className="card-title p-4 px-lg-3" >{item.name}</h2>
+                <h2 className="card-title p-4 px-lg-3">{item.name}</h2>
             </div>
         </div>
     );
