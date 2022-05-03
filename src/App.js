08481e49779710c0f71e107b202b40a5ce69cb8c
danieljporter21 <danieljporter21@googlemail.com>
2022-05-03 09:19:04 +0100

    updated jumbotron so image fits correctly

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Jumbo from './Components/Jumbo'
+import {useState} from "react";
 
 const App = () => {
+
+  const jumboHeaderHome = 'Food. Delivered.';
+  const jumboTextHome = 'Order your favourite food from local restaurants, right to your door.'
+
   return (
     <div className="App">
       <Header />
--- src/Components/Jumbo/index.js
+++ src/Components/Jumbo/index.js
@@ -1,21 +1,23 @@
 import jumboimg from './burgers.jpg';
 import jumboimgmd from './burgers-md.jpg';
 
-const Jumbo = () => {
+const Jumbo = (props) => {
+
   return (
-  <div className="card border-0 p-0 bg-primary rounded-0 m-3">
-    <div className="row m-0">
-      <picture className="p-0">
-        <source srcSet={jumboimgmd} media="(min-width: 768px)"/>
-        <img src={jumboimg} className="card-img-top p-0" alt=""/>
-      </picture>
-      <div className={"card-img-overlay col-lg-5 col-11 h-50 m-auto bg-white bg-opacity-75 rounded-0 "}>
-        <h1 className="card-title m-0 p-0 display-4 text-info text-center"><strong>Food. Delivered</strong></h1>
-        <p className="card-text m-0 p-0 text-center">Order your favourite food from local restaurants, right to your door.</p>
+    <div className="card border-0 p-0 bg-primary rounded-0">
+      <div className="row m-0">
+        <picture className="p-0">
+          <source srcSet={jumboimgmd}
+                  media="(min-width: 768px)"/>
+          <img src={jumboimg} className="card-img-top p-0" alt=""/>
+        </picture>
+        <div className={"card-img-overlay col-lg-6 col-10 h-50 m-auto bg-white bg-opacity-75 rounded-0 p-5"}>
+          <h1 className="card-title m-0 p-0 display-4 text-info"><strong>Food. Delivered.</strong></h1>
+          <p className="card-text m-0 p-0">Order your favourite food from local restaurants, right to your door.</p>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Jumbo;
