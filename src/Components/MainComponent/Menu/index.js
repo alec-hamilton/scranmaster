636f065ef8a6ba84e6772af58b1f62e80751fd2c
alech <hamiltonalec@gmail.com>
2022-05-04 16:32:09 +0100

    plus and minus buttons added

--- src/Components/MainComponent/Menu/index.js
+++ src/Components/MainComponent/Menu/index.js
@@ -1,6 +1,47 @@
 import FoodItem from "./FoodItem";
+import {useEffect, useState} from "react";
 
 const Menu = ({menuItems, showingMenuItems}) => {
+
+    const [orderItems, setOrderItems] = useState([]);
+
+    const addToOrderItems = (orderItem) => {
+
+        const filterItems = (item) => {
+            return item.name === orderItem.name;
+        }
+
+        const index = orderItems.findIndex(filterItems);
+
+        if (index === -1) {
+            setOrderItems([...orderItems, orderItem]);
+        } else {
+            orderItems[index].quantity += 1;
+            setOrderItems([...orderItems]);
+        }
+    }
+
+    const subtractFromOrderItems = (orderItem) => {
+
+        const filterItems = (item) => {
+            return item.name === orderItem.name;
+        }
+
+        const index = orderItems.findIndex(filterItems);
+
+        if (index !== -1) {
+            orderItems[index].quantity -= 1;
+
+            if (orderItems[index].quantity === 0) {
+                orderItems.splice(index,1);
+            }
+            setOrderItems([...orderItems]);
+        }
+    }
+
+    useEffect(() => {
+        console.log(orderItems);
+    }, [orderItems])
 
     if (menuItems.foodItems === undefined) {
         return (
@@ -12,10 +53,14 @@ const Menu = ({menuItems, showingMenuItems}) => {
     return (
         <div className={showingMenuItems} >
             <div className="d-flex flex-wrap justify-content-start">
-                {menuItems.foodItems.map((foodItems, index) => {
+                {menuItems.foodItems.map((foodItem, index) => {
                         return (
-                            <div className="col-12 col-lg-2 px-1 my-1" key={index}>
-                                <FoodItem foodItems={foodItems}/>
+                            <div className="col-12 col-lg-2 px-1 my-1 card-group" key={index}>
+                                <FoodItem foodItem={foodItem}
+                                          addToOrderItems={addToOrderItems}
+                                          orderItems={orderItems}
+                                          subtractFromOrderItems={subtractFromOrderItems}
+                                />
                             </div>
                         );
                     }
