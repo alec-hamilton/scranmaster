68daacf0a5e55a2a5057e756bb940d2a6de7ba4a
alech <hamiltonalec@gmail.com>
2022-05-05 12:07:51 +0100

    trainer review changes

--- src/Components/MainComponent/Menu/index.js
+++ src/Components/MainComponent/Menu/index.js
@@ -33,15 +33,11 @@ const Menu = ({menuItems, showingMenuItems}) => {
         if (index !== -1) {
             orderItems[index].quantity -= 1;
             if (orderItems[index].quantity === 0) {
-                orderItems.splice(index,1);
+                orderItems.splice(index, 1);
             }
             setOrderItems([...orderItems]);
         }
     }
-
-    useEffect(() => {
-        console.log(orderItems);
-    }, [orderItems])
 
     if (menuItems.foodItems === undefined) {
         return (
@@ -51,27 +47,25 @@ const Menu = ({menuItems, showingMenuItems}) => {
     }
 
     return (
-        <>
-            <div className={showingMenuItems} >
-                <div className="d-flex flex-column flex-lg-row ">
-                    <div className="d-flex flex-wrap justify-content-start col-12 col-lg-10">
-                        {menuItems.foodItems.map((foodItem, index) => {
-                                return (
-                                    <div className="col-12 col-lg-2 px-1 my-1 card-group" key={index}>
-                                        <FoodItem foodItem={foodItem}
-                                                  addToOrderItems={addToOrderItems}
-                                                  orderItems={orderItems}
-                                                  subtractFromOrderItems={subtractFromOrderItems}
-                                        />
-                                    </div>
-                                );
-                            }
-                        )}
-                    </div>
-                    <OrderList className="col-lg-2 col-12" orderItems={orderItems}/>
+        <div className={showingMenuItems}>
+            <div className="d-flex flex-column flex-lg-row ">
+                <div className="d-flex flex-wrap justify-content-start col-12 col-lg-10">
+                    {menuItems.foodItems.map((foodItem, index) => {
+                            return (
+                                <div className="col-12 col-lg-2 px-1 my-1 card-group" key={index}>
+                                    <FoodItem foodItem={foodItem}
+                                              addToOrderItems={addToOrderItems}
+                                              orderItems={orderItems}
+                                              subtractFromOrderItems={subtractFromOrderItems}
+                                    />
+                                </div>
+                            );
+                        }
+                    )}
                 </div>
+                <OrderList className="col-lg-2 col-12" orderItems={orderItems}/>
             </div>
-        </>
+        </div>
     );
 }
 
